ba0fa8611c71a1e261e2a2639af0016a6fe2e2bc
Trooper M <migmon23@hotmail.com>
2021-02-10 14:07:06 -0500

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace CashRegister
+{
+    partial class securitySystemsShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(securitySystemsShop));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberAlarmsLabel = new System.Windows.Forms.Label();
+            this.numberMotionScannerLabel = new System.Windows.Forms.Label();
+            this.securityCamerasInput = new System.Windows.Forms.TextBox();
+            this.motionScannerInput = new System.Windows.Forms.TextBox();
+            this.alarmsInput = new System.Windows.Forms.TextBox();
+            this.calculateTotalsButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subTotalOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.lineLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.ChangeLabel = new System.Windows.Forms.Label();
+            this.changedOutput = new System.Windows.Forms.Label();
+            this.printReceiptButten = new System.Windows.Forms.Button();
+            this.receiptOutput = new System.Windows.Forms.Label();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.titleLabel.Location = new System.Drawing.Point(0, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(524, 37);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Security Systems Shop (S.S.S)";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Security Cameras:";
+            // 
+            // numberAlarmsLabel
+            // 
+            this.numberAlarmsLabel.AutoSize = true;
+            this.numberAlarmsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberAlarmsLabel.Location = new System.Drawing.Point(12, 86);
+            this.numberAlarmsLabel.Name = "numberAlarmsLabel";
+            this.numberAlarmsLabel.Size = new System.Drawing.Size(109, 15);
+            this.numberAlarmsLabel.TabIndex = 2;
+            this.numberAlarmsLabel.Text = "Number of Alarms:";
+            // 
+            // numberMotionScannerLabel
+            // 
+            this.numberMotionScannerLabel.AutoSize = true;
+            this.numberMotionScannerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberMotionScannerLabel.Location = new System.Drawing.Point(12, 113);
+            this.numberMotionScannerLabel.Name = "numberMotionScannerLabel";
+            this.numberMotionScannerLabel.Size = new System.Drawing.Size(158, 15);
+            this.numberMotionScannerLabel.TabIndex = 3;
+            this.numberMotionScannerLabel.Text = "Number of Motion Scanner:";
+            // 
+            // securityCamerasInput
+            // 
+            this.securityCamerasInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.securityCamerasInput.Location = new System.Drawing.Point(185, 57);
+            this.securityCamerasInput.Name = "securityCamerasInput";
+            this.securityCamerasInput.Size = new System.Drawing.Size(44, 21);
+            this.securityCamerasInput.TabIndex = 4;
+            // 
+            // motionScannerInput
+            // 
+            this.motionScannerInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.motionScannerInput.Location = new System.Drawing.Point(185, 110);
+            this.motionScannerInput.Name = "motionScannerInput";
+            this.motionScannerInput.Size = new System.Drawing.Size(44, 21);
+            this.motionScannerInput.TabIndex = 5;
+            // 
+            // alarmsInput
+            // 
+            this.alarmsInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alarmsInput.Location = new System.Drawing.Point(185, 84);
+            this.alarmsInput.Name = "alarmsInput";
+            this.alarmsInput.Size = new System.Drawing.Size(44, 21);
+            this.alarmsInput.TabIndex = 6;
+            // 
+            // calculateTotalsButton
+            // 
+            this.calculateTotalsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateTotalsButton.Location = new System.Drawing.Point(57, 153);
+            this.calculateTotalsButton.Name = "calculateTotalsButton";
+            this.calculateTotalsButton.Size = new System.Drawing.Size(122, 34);
+            this.calculateTotalsButton.TabIndex = 7;
+            this.calculateTotalsButton.Text = "Calculate Totals";
+            this.calculateTotalsButton.UseVisualStyleBackColor = true;
+            this.calculateTotalsButton.Click += new System.EventHandler(this.CalculateTotalsButton_Click);
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalLabel.Location = new System.Drawing.Point(12, 207);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(63, 19);
+            this.subTotalLabel.TabIndex = 8;
+            this.subTotalLabel.Text = "Sub Total:";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(12, 245);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(30, 15);
+            this.taxLabel.TabIndex = 9;
+            this.taxLabel.Text = "Tax:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(12, 289);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(37, 15);
+            this.totalLabel.TabIndex = 10;
+            this.totalLabel.Text = "Total:";
+            // 
+            // subTotalOutput
+            // 
+            this.subTotalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalOutput.Location = new System.Drawing.Point(154, 207);
+            this.subTotalOutput.Name = "subTotalOutput";
+            this.subTotalOutput.Size = new System.Drawing.Size(73, 19);
+            this.subTotalOutput.TabIndex = 11;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxOutput.Location = new System.Drawing.Point(154, 245);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(73, 15);
+            this.taxOutput.TabIndex = 12;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalOutput.Location = new System.Drawing.Point(154, 289);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(73, 15);
+            this.totalOutput.TabIndex = 13;
+            // 
+            // lineLabel
+            // 
+            this.lineLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lineLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lineLabel.Location = new System.Drawing.Point(12, 314);
+            this.lineLabel.Name = "lineLabel";
+            this.lineLabel.Size = new System.Drawing.Size(215, 10);
+            this.lineLabel.TabIndex = 14;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(12, 335);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(72, 18);
+            this.tenderedLabel.TabIndex = 15;
+            this.tenderedLabel.Text = "Tendered:";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedInput.Location = new System.Drawing.Point(154, 332);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(73, 21);
+            this.tenderedInput.TabIndex = 16;
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateChangeButton.Location = new System.Drawing.Point(57, 363);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(122, 34);
+            this.calculateChangeButton.TabIndex = 17;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            this.calculateChangeButton.Click += new System.EventHandler(this.CalculateChangeButton_Click);
+            // 
+            // ChangeLabel
+            // 
+            this.ChangeLabel.AutoSize = true;
+            this.ChangeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeLabel.Location = new System.Drawing.Point(12, 400);
+            this.ChangeLabel.Name = "ChangeLabel";
+            this.ChangeLabel.Size = new System.Drawing.Size(53, 15);
+            this.ChangeLabel.TabIndex = 18;
+            this.ChangeLabel.Text = "Change:";
+            // 
+            // changedOutput
+            // 
+            this.changedOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changedOutput.Location = new System.Drawing.Point(157, 400);
+            this.changedOutput.Name = "changedOutput";
+            this.changedOutput.Size = new System.Drawing.Size(70, 15);
+            this.changedOutput.TabIndex = 19;
+            // 
+            // printReceiptButten
+            // 
+            this.printReceiptButten.Cursor = System.Windows.Forms.Cursors.Default;
+            this.printReceiptButten.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printReceiptButten.Location = new System.Drawing.Point(57, 431);
+            this.printReceiptButten.Name = "printReceiptButten";
+            this.printReceiptButten.Size = new System.Drawing.Size(122, 34);
+            this.printReceiptButten.TabIndex = 20;
+            this.printReceiptButten.Text = "Print Receipt ";
+            this.printReceiptButten.UseVisualStyleBackColor = true;
+            this.printReceiptButten.Click += new System.EventHandler(this.PrintReceiptButten_Click);
+            // 
+            // receiptOutput
+            // 
+            this.receiptOutput.BackColor = System.Drawing.Color.White;
+            this.receiptOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptOutput.Location = new System.Drawing.Point(251, 57);
+            this.receiptOutput.Name = "receiptOutput";
+            this.receiptOutput.Size = new System.Drawing.Size(252, 374);
+            this.receiptOutput.TabIndex = 21;
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.newOrderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newOrderButton.Location = new System.Drawing.Point(318, 442);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(122, 34);
+            this.newOrderButton.TabIndex = 22;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = true;
+            this.newOrderButton.Click += new System.EventHandler(this.NewOrderButton_Click);
+            // 
+            // securitySystemsShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(525, 488);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.receiptOutput);
+            this.Controls.Add(this.printReceiptButten);
+            this.Controls.Add(this.changedOutput);
+            this.Controls.Add(this.ChangeLabel);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.lineLabel);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.subTotalOutput);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.calculateTotalsButton);
+            this.Controls.Add(this.alarmsInput);
+            this.Controls.Add(this.motionScannerInput);
+            this.Controls.Add(this.securityCamerasInput);
+            this.Controls.Add(this.numberMotionScannerLabel);
+            this.Controls.Add(this.numberAlarmsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "securitySystemsShop";
+            this.Text = "Security Systems Shop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label numberAlarmsLabel;
+        private System.Windows.Forms.Label numberMotionScannerLabel;
+        private System.Windows.Forms.TextBox securityCamerasInput;
+        private System.Windows.Forms.TextBox motionScannerInput;
+        private System.Windows.Forms.TextBox alarmsInput;
+        private System.Windows.Forms.Button calculateTotalsButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subTotalOutput;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label lineLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.Label ChangeLabel;
+        private System.Windows.Forms.Label changedOutput;
+        private System.Windows.Forms.Button printReceiptButten;
+        private System.Windows.Forms.Label receiptOutput;
+        private System.Windows.Forms.Button newOrderButton;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace CashRegister
+{
+    public partial class securitySystemsShop : Form
+    {
+        double securityCamerasPrice = 22.99;
+        double alarmsPrice = 82.48;
+        double motionScannerPrice = 15.00;
+        int numberOfSecurityCameras;
+        int numberOfAlarms;
+        int numberOfMotionScanners;
+        double totalCost;
+        double tax = 0.13;
+        double taxAmount;
+        double totalTaxCost;
+        double tenderedAmount;
+        double change;
+
+        public securitySystemsShop()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTotalsButton_Click(object sender, EventArgs e)
+        {
+            numberOfSecurityCameras = Convert.ToInt32(securityCamerasInput.Text);
+            numberOfAlarms = Convert.ToInt32(alarmsInput.Text);
+            numberOfMotionScanners = Convert.ToInt32(motionScannerInput.Text);
+
+            totalCost = securityCamerasPrice * numberOfSecurityCameras + alarmsPrice * numberOfAlarms + motionScannerPrice * numberOfMotionScanners;
+            taxAmount = totalCost * tax;
+            totalTaxCost = totalCost + taxAmount;
+
+            subTotalOutput.Text = $"{totalCost.ToString("$.00")}";
+            taxOutput.Text = $"{taxAmount.ToString("$.00")}";
+            totalOutput.Text = $"{totalTaxCost.ToString("$.00")}";
+
+
+        }
+
+        private void CalculateChangeButton_Click(object sender, EventArgs e)
+        {
+            tenderedAmount = Convert.ToInt32(tenderedInput.Text);
+            change = tenderedAmount - totalTaxCost;
+            changedOutput.Text = $"{change.ToString("$.00")}";
+        }
+
+        private void NewOrderButton_Click(object sender, EventArgs e)
+        {
+            securityCamerasInput.Text = "0";
+            alarmsInput.Text = "0";
+            motionScannerInput.Text = "0";
+
+            subTotalOutput.Text = "$0.00";
+            taxOutput.Text = "$0.00";
+            totalOutput.Text = "$0.00";
+
+            tenderedInput.Text = "0";
+            changedOutput.Text = "$0.00";
+        }
+
+        private void PrintReceiptButten_Click(object sender, EventArgs e)
+        {
+            receiptOutput.Text = $"\n\n                     Security Systems Shop";
+            Thread.Sleep(2000);
+           // receiptOutput.Text = $"\n "
+        }
+    }
+}
